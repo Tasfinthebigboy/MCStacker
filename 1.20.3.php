056d7ff19d3bf97f2bcc5e852a44a2480b108d11
Tasfinthebigboy <96976817+Tasfinthebigboy@users.noreply.github.com>
2024-05-04 13:41:52 +0600

    Create 1.20.3.php

---
+++ 1.20.3.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-10KHEXXM4G"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-10KHEXXM4G');
+</script><title>MCStacker for MC 1.20.3/1.20.4</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="icon" href="icon.svg" sizes="any" type="image/svg+xml">
+<link rel="stylesheet" type="text/css" href="alertify.core.css">
+<link rel="stylesheet" type="text/css" href="alertify.default.css">
+<link rel="stylesheet" type="text/css" href="mcstacker-common.css">
+
+<link id='cssTheme' rel='stylesheet' type='text/css' href='mcstacker-darker.css'>
+<script language="JavaScript" src="alertify.min.js" type="text/javascript"></script>
+<script language="JavaScript" src="jscolor/jscolor.js" type="text/javascript"></script>
+
+<script src="jquery-1.12.4.min.js"></script>
+<script src="jquery-ui.min.js"></script>
+<script language="JavaScript" src="mcstacker-1.20.3.min.js" type="text/javascript"></script>
+<script language="JavaScript" src="jquery.ui.touch-punch.min.js" type="text/javascript"></script>
+</head>
+<body>
+<div id='topPart'>
+<br><div class='ads'>    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6210527081945025"
+    crossorigin="anonymous"></script>
+<!-- MCStacker 2 728x90 -->
+<ins class="adsbygoogle"
+    style="display:inline-block;width:728px;height:90px"
+    data-ad-client="ca-pub-6210527081945025"
+    data-ad-slot="4710945914"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script><BR> <br>Become an MCStacker Patreon to make these ads disappear. Head over to <a href='https://www.patreon.com/mcstacker'>patreon.com/mcstacker</a> to get the ad-free link.</div><span id='logo'><a href='https://mcstacker.net' target=_blank><img src='MCSLogo.svg' style="width:400px;padding:5px" alt='MCStacker'></a></span>
+	<span id='socialandnav'>
+	<div id='socialbuts'>
+			<a href='https://discord.gg/WCb6GNf' title='Discuss all things MCStacker on the MCStacker Discord Server' target=_blank><img src='buts/discord.svg' height=40></a>
+			<a href='https://bsky.app/profile/mcstacker.net' title='MCStacker on BlueSky' target=_blank><img src='buts/Bluesky_butterfly-logo.svg' height=40></a>
+			<a href='https://www.patreon.com/mcstacker/' title='Support the MCStacker project on Patreon' target=_blank><img src='buts/PATREON_SYMBOL_1_WHITE_RGB.svg' height=40 style="filter: drop-shadow(2px 0 0 #111) drop-shadow(0 2px 0 #111) drop-shadow(-2px 0 0 #111) drop-shadow(0 -2px 0 #111);"></a>
+			<a href='https://ko-fi.com/mcstacker' title='Support the MCStacker project on Ko-fi' target=_blank><img src='buts/ko-fi.svg' height=40></a>
+			
+		</div>
+		<span id='nav'>
+			<a href="javascript:loadForm('bossbar')" class='mainButtons combut'>/bossbar</a>
+			<a href="javascript:loadForm('damage')" class='mainButtons combut'>/damage</a>
+			
+			<a href="javascript:loadForm('data merge block')" class='mainButtons combut'>/data merge block</a>
+			<a href="javascript:loadForm('data merge entity')" class='mainButtons combut'>/data merge entity</a> 
+			<a href="javascript:loadForm('effect')" class='mainButtons combut'>/effect</a>
+			<a href="javascript:loadForm('execute if entity')" class='mainButtons combut'>/execute if entity</a>
+			<a href="javascript:loadForm('fill')" class='mainButtons combut'>/fill</a>
+			<a href="javascript:loadForm('give')" class='mainButtons combut'>/give</a> 
+			<a href="javascript:loadForm('item')" class='mainButtons combut'>/item</a>
+			<a href="javascript:loadForm('kill')" class='mainButtons combut'>/kill</a> 
+			<a href="javascript:loadForm('loot')" class='mainButtons combut'>/loot</a>
+			<a href="javascript:loadForm('particle')" class='mainButtons combut'>/particle</a>
+			<a href="javascript:loadForm('playsound')" class='mainButtons combut'>/playsound</a>
+			<a href="javascript:loadForm('ride')" class='mainButtons combut'>/ride</a>
+			<a href="javascript:loadForm('scoreboard')" class='mainButtons combut'>/scoreboard</a>
+			<a href="javascript:loadForm('setblock')" class='mainButtons combut'>/setblock</a> 
+			<a href="javascript:loadForm('summon')" class='mainButtons combut'>/summon</a>
+			<a href="javascript:loadForm('team')" class='mainButtons combut'>/team</a>
+			<a href="javascript:loadForm('teleport')" class='mainButtons combut'>/teleport</a> 
+			<a href="javascript:loadForm('tellraw')" class='mainButtons combut'>/tellraw</a>
+			<a href="javascript:loadForm('title')" class='mainButtons combut'>/title</a>
+			<select id='homePageCommandChooser' class='mainButtons'></select>
+		</span>
+		<span id='otherNav'>
+			<a href="javascript:loadForm('loot_table')" class='mainButtons specialButton'>Loot Tables</a>
+			<a href="javascript:showCommunity()" class='mainButtons specialButton'>Saved Commands</a>
+			<a href="javascript:showClipboard()" class='mainButtons specialButton'>Clipboard</a>
+			<a href="javascript:showImport()" class='mainButtons specialButton'>Import</a>
+			<a href="javascript:showSettings()" class='mainButtons specialButton'>Settings</a>
+			<a href="javascript:loadVersions()" class='mainButtons specialButton'>Versions</a>
+			<a href="https://ko-fi.com/s/acc666ea82" class='mainButtons specialButton'>Offline Version</a>
+			<a href="javascript:loadSupporters()" class='mainButtons specialButton'>Supporters</a>
+			
+		</span>
+	</span>
+</div>
+<div id='community'>
+	<div id='navPane'>NAV</div><div id='manPane'>manPane</div>
+	</div>
+	<div id=intro>
+	
+	<h1>MCStacker for Minecraft Java Edition 1.20.3/1.20.4</h1>
+	<p style="width:600px">
+	Check out MCStacker on the <a href='https://www.patreon.com/mcstacker' target=_blank>Patreon Page</a> and please support this project if you can. As always requests, comments, feedback and bug reports are welcome at <a href='mailto:bimbimma@gmail.com' target=_blank>bimbimma@gmail.com</a> or on the<a href='https://discord.gg/WCb6GNf' title='Discuss all things MCStacker on the MCStacker Discord Server' target=_blank> MCStacker Discord Server</a></p>
+	</p>
+	<p style="width:600px">If you are looking for alternative versions of MCStacker, you can find them <a href="javascript:loadVersions()">all here</a>.</p>
+	
+	<br><div class='ads'>    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6210527081945025"
+    crossorigin="anonymous"></script>
+<!-- MCStacker 2 728x90 -->
+<ins class="adsbygoogle"
+    style="display:inline-block;width:728px;height:90px"
+    data-ad-client="ca-pub-6210527081945025"
+    data-ad-slot="4710945914"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script><BR> </div>	<div style="display: flex;">
+			<div><script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#29abe0', 'B0B2BOMSM');kofiwidget2.draw();</script><br><br>
+				<a href='npc/' title='Check out the Minecraft NPC Generator' target=_blank class='bigads'><img src='npc_generator.png' alt='Check out the Interactive NPC Generator'></a>
+				<a href='https://www.patreon.com/mcstacker' title='Support MCStacker Development on Patreon' target=_blank class='bigads'><img src='PATREON_WORDMARK_1_BLACK_RGB.svg' width=216 alt='Support MCStacker Development on Patreon'></a>
+				<br><a href='https://wcw.mcstacker.net/' title='Play on the Weaponized Creeper Wars Server' target=_blank class='bigads'><img src='WCWad.png' alt='Play on the Weaponized Creeper Wars Server'></a>
+				<a href='murals/' title='Turn images into big murals in minecraft' target=_blank class='bigads'><img src='minecraft_mural_generator.png' alt='Check out Minecraft Mural Generator'></a>
+
+			</div>
+
+		</div>
+	</div>
+	<BR>
+
+		<div tabindex="0"  id="dialogbox"></div>
+		<div id='commandForm'></div>
+		<br><div class='ads'>    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6210527081945025"
+    crossorigin="anonymous"></script>
+<!-- MCStacker 2 728x90 -->
+<ins class="adsbygoogle"
+    style="display:inline-block;width:728px;height:90px"
+    data-ad-client="ca-pub-6210527081945025"
+    data-ad-slot="4710945914"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script><BR> <br>Become an MCStacker Patreon to make these ads disappear. Head over to <a href='https://www.patreon.com/mcstacker'>patreon.com/mcstacker</a> to get the ad-free link.</div>		<div id="commandOutput" class="ui-widget-content" onMouseOver="this.style.cursor='pointer'">
+			<span id='outputLength'></span><br>
+			<textarea id='commandOutput-ta'></textarea>
+			<input type='checkbox' id='disableOutput'>Disable Output&nbsp;&nbsp;
+			<a href='javascript:generateCommand(true)' class='greenPlus'>Refresh</a>
+			<span id='toClipBoard' class='greenPlus'>Copy</span>
+		</div>
+		<div id="commandInput" class="ui-widget-content" onMouseOver="this.style.cursor='pointer'">
+			Import Command<span id='closeImportBut' class='redX'>X</span>
+			<textarea id='commandInput-ta' placeholder="Paste a command here."></textarea>
+			<span id='parseNBT' class='greenPlus'>Import</span><span id='clearNBT' class='greenPlus'>Clear</span>
+		</div>
+		<div id="clipBoard" class="ui-widget-content" onMouseOver="this.style.cursor='pointer'">
+			MCStacker Internal Clipboard<span id='closeclipBoardBut' class='redX'>X</span><br>
+			<textarea id='clipBoard-ta' placeholder="Paste NBT here."></textarea>Content<select id='nbtType' onChange='loadCBType()'><option value='0'>Item</option><option value='1'>Entity</option><option value='2'>Trade</option><option value='3'>Text Components</option></select>
+		</div>
+		<div id="settings" class="ui-widget-content" onMouseOver="this.style.cursor='pointer'">	Settings<span id='closeSettingsBut' class='redX'>X</span>
+		<div id='settingsArea'>
+				<span class='fLabel200'>Command Prefix</span> <input type='text' id='commandPrefix' class='bigTF'><a href="javascript:showTip('settings.commandPrefix')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Update Prefix on Import</span><input type='checkbox' id='updateCommandPrefix'><a href="javascript:showTip('settings.updateCommandPrefix')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Namespace</span> <input type='text' id='namespace' class='bigTF'><a href="javascript:showTip('settings.namespace')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Update Namespace on Import</span><input type='checkbox' id='updateNamespace'><a href="javascript:showTip('settings.updateNamespace')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Remove Command Slash</span><input type='checkbox' id='removeSlash'><a href="javascript:showTip('settings.removeSlash')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Load Item User Interface</span><input type='checkbox' id='loadItemUI'><a href="javascript:showTip('settings.loadItemUI')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Randomize Modifier UUIDs</span><input type='checkbox' id='randmizeUUIDs'><a href="javascript:showTip('settings.randmizeUUIDs')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Warn on Close/Reload</span><input type='checkbox' id='warnOnClose'><a href="javascript:showTip('settings.warnOnClose')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Website Theme</span><select id='themeChooser'><!--<option value='april_fool.css'>New Default</option>--><option value='mcstacker-darker.css'>Dark (Orange)</option><option value='mcstacker-darker-green.css'>Dark (Green)</option><option value='dark-original.css'>Dark (Original)</option><option value='mcstacker-light.css'>Light</option><option value='light-original.css'>Light (Original)</option></select><a href="javascript:showTip('settings.themeChooser')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Item/Block List Language</span><select id='langChooser'>
+					<option value='en'>en</option>
+					<option value='es'>es</option>
+					<option value='de'>de</option>
+					<option value='fr'>fr</option>
+					<option value='it'>it</option>
+					<option value='ru'>ru</option>
+					<option value='ja'>ja</option>
+					<option value='ko'>ko</option>
+					<option value='zh'>zh</option>
+					<option value='ids'>IDs</option>
+				</select><a href="javascript:showTip('settings.langChooser')" class="greenPlus" tabindex="-1">?</a><br>
+				<span class='fLabel200'>Item/Block List Sort Order</span><select id='sortMode'><option value='item_id'>IDs A-Z</option><option value='item_desc'>Name A-Z</option><option value='desc_length'>Length, Name A-Z</option></select><a href="javascript:showTip('settings.sortMode')" class="greenPlus" tabindex="-1">?</a><br>
+			</div>
+		</div>
+	</body>
+</html>
